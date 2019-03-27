e198ced588364e48f2418354e440250c4ebaf417
YuTengjing <2713151713@qq.com>
2019-03-27 13:40:09 +0800

    Refactor: optimize the DeviceList ScrollBar

--- src/components/deviceList/DevicesList.jsx
+++ src/components/deviceList/DevicesList.jsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
+import { Scrollbars } from 'react-custom-scrollbars';
 import './DeviceList.scss';
 
 export const DeviceList = (props) => {
     const { deviceListData } = props;
-    
+
     const renderDeviceList = () => deviceListData.map((device, index) => {
         const { imageSrc, model } = device;
 
@@ -19,15 +20,20 @@ export const DeviceList = (props) => {
 
     return (
         <div className="device-list">
-            <div className="device-list-body">
-                {renderDeviceList()}
-            </div>
-            
+            <Scrollbars
+                style={{ width: 860, height: 528, borderBottom: '1px solid rgb(32, 178, 170)'}}
+                autoHide
+                >
+                <div className="device-list-body">
+                    {renderDeviceList()}
+                </div>
+            </Scrollbars>
             <ReadMore
                 text={'查看更多'}
                 disabled={false}
             />
         </div>
+
     );
 };
 
@@ -35,7 +41,7 @@ const ReadMore = (props) => {
     const { text, disabled } = props;
 
     return (
-        <div className={ `read-more ${ disabled ? 'read-more-disable' : ''}` } >
+        <div className={`read-more ${disabled ? 'read-more-disable' : ''}`} >
             <span>{text}</span>
         </div>
     );
@@ -53,9 +59,9 @@ DeviceList.propTypes = {
 const DeviceListItem = (props) => {
     const { imageSrc, model } = props;
 
-    
+
     return (
-        <div className="model-wrapper hvr-back-pulse hvr-grow-shadow" >
+        <div className="model-wrapper" >
             <img src={imageSrc} alt={model} />
             <span>{model}</span>
         </div>
